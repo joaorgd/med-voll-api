2dc6ef5423a397520d681ec0e386018d65d45fec
joaorgd <joaorobertoguedesdantas@gmail.com>
2025-08-05 13:02:20 -0300

    feat(medico): Implementa validações e transacionalidade no cadastro

--- src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -1,10 +1,21 @@
 package med.voll.api.endereco;
 
-public record DadosEndereco(String logradouro,
-                            String bairro,
-                            String cep,
-                            String cidade,
-                            String uf,
-                            String complemento,
-                            String numero) {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosEndereco(
+        @NotNull
+        String logradouro,
+        @NotNull
+        String bairro,
+        @NotNull
+        @Pattern(regexp = "\\d{8}")
+        String cep,
+        @NotNull
+        String cidade,
+        @NotNull
+        String uf,
+        @NotNull
+        String complemento,
+        String numero) {
 }
--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,10 +1,23 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
-public record DadosCadastroMedico(String nome,
-                                  String email,
-                                  String crm,
-                                  Especialidade especialidade,
-                                  DadosEndereco endereco) {
+public record DadosCadastroMedico(
+
+        @NotBlank // Verifica se uma String não é nula e não é branca.
+        String nome,
+        @NotBlank
+        String email,
+        @NotNull // Verifica se o campo não é nulo.
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+        @NotNull
+        @Valid
+        DadosEndereco endereco) {
 }
