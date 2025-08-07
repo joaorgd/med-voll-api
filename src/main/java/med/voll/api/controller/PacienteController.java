22b0fd99b1775b3175c41be5ab00d1b44a15c086
joaorgd <joaorobertoguedesdantas@gmail.com>
2025-08-07 10:46:48 -0300

    Criação de classes paciente e tratamento de erro 500

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,7 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.paciente.*; // Supondo que as classes de paciente estejam neste pacote
+import med.voll.api.domain.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
