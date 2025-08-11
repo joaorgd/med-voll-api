3dbfbbbd2b5adf146ac968dd5d6d44a5e658197d
joaorgd <joaorobertoguedesdantas@gmail.com>
2025-08-11 12:18:20 -0300

    refactor(consulta): Abstrai validadores para uma interface comum

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
@@ -2,18 +2,20 @@ package med.voll.api.domain.consulta.validacoes;
 
 import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-public class ValidadorHorarioAntecedencia {
+@Component
+public class ValidadorHorarioAntecedencia implements ValidadorAgendamentoDeConsulta {
 
-    public void validar(DadosAgendamentoConsulta dados){
+    public void validar(DadosAgendamentoConsulta dados) {
         var dataConsulta = dados.data();
         var agora = LocalDateTime.now();
         var diferencaEmMinutos = Duration.between(agora, dataConsulta).toMinutes();
 
-        if(diferencaEmMinutos < 30) {
+        if (diferencaEmMinutos < 30) {
             throw new ValidacaoException("As consultas devem ser marcadas com uma antecedência mínima de 30 minutos.");
         }
     }
