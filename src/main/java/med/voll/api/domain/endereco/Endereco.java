6e456378f4a148b35cc15d822a394f8de032057e
joaorgd <joaorobertoguedesdantas@gmail.com>
2025-08-12 12:02:07 -0300

    test(domain): Implementa testes para repositórios e controllers

--- src/main/java/med/voll/api/domain/endereco/Endereco.java
+++ src/main/java/med/voll/api/domain/endereco/Endereco.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = "id")
+@EqualsAndHashCode
 public class Endereco {
     private String logradouro;
     private String bairro;
